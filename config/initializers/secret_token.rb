681f894e9d4aec43f9201faac4450cf69cc55375
Alexander Osmanov <grandison@mail.ru>
2013-10-05 12:50:41 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialUp::Application.config.secret_token = '3a0ffbbc67c1d15af4c48167d0c31f5bfd35f7d753ecb3c12a6d72e0cde7516d133308a7adc893753f546c9bc722a5d294e5478ac1dc270b5c87288d04034be5'
