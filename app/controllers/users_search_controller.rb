22c586c6a7b097144fb87bf9fd5d7c9f72d06d26
Ruslan Hamidullin <ror_developer@mail.ru>
2013-10-06 11:36:25 +0400

    [ Main page ] done

--- app/controllers/users_search_controller.rb
+++ app/controllers/users_search_controller.rb
@@ -4,8 +4,10 @@ class UsersSearchController < ApplicationController
       with_alarms.
       by_name(params[:term])
 
-    Rails.logger.info "--------#{@users.count}"
-
     render json: @users
+  end
+
+  def render_user
+    @user = current_user.friends.with_alarms.find(params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ SocialUp::Application.routes.draw do
     end
   end
   
-  resources :users_search, only: [ :index ]
+  resources :users_search, only: [ :index ] do
+    get :render_user, on: :collection
+  end
   
   namespace :api do
     namespace :v1 do
