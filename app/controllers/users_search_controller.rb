85d8377eebc87e7186cb52d6291e5e4ee73fb6a5
Alexander Osmanov <grandison@mail.ru>
2013-10-06 17:02:46 +0400

    Show all friends

--- app/controllers/users_search_controller.rb
+++ app/controllers/users_search_controller.rb
@@ -1,13 +1,12 @@
 class UsersSearchController < ApplicationController
   def index
     @users = current_user.friends.
-      with_alarms.
       by_name(params[:term])
 
     render json: @users
   end
 
   def render_user
-    @user = current_user.friends.with_alarms.find(params[:user_id])
+    @user = current_user.friends.find(params[:user_id])
   end
 end
